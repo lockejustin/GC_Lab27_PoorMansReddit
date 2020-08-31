821ef17bc54bdb825b26cbed4b89b82bb58656e1
justin <justin.d.locke@gmail.com>
2020-08-31 17:24:16 -0400

    initial commit

---
+++ GC_Lab27_PoorMansReddit/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GC_Lab27_PoorMansReddit.Models;
+
+namespace GC_Lab27_PoorMansReddit.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            RedditDAL dl = new RedditDAL();
+
+            string Json = dl.CallAPI();
+
+            ViewBag.data = Json;
+
+            return View();
+        }
+
+        public IActionResult RedditPostData()
+        {
+            RedditDAL dl = new RedditDAL();
+            RedditPost p = dl.GetRedditPost();
+
+            return View(p);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GC_Lab27_PoorMansReddit/Models/RedditDAL.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GC_Lab27_PoorMansReddit.Models
+{
+    public class RedditDAL
+    {
+        public string CallAPI()
+        {
+            //sets up our request
+            HttpWebRequest request = WebRequest.CreateHttp("https://www.reddit.com/r/aww/.json?limit=10");
+
+            //setup would go here if necessary
+
+            //sends request to remote server and gets response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //sets up streamreader
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            //readtoend reads all data
+            string output = rd.ReadToEnd();
+
+            return output;
+        }
+
+        public RedditPost GetRedditPost()
+        {
+            string PersonJason = CallAPI();
+
+            //Takes the Json data and puts it inot a Json object
+            JObject json = JObject.Parse(PersonJason);
+
+
+            RedditPost p = JsonConvert.DeserializeObject<RedditPost>(json.ToString());
+
+            return p;
+        }
+    }
+}
